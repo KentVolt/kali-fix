59fc48123506c74dbc45ac24d3acd4413c0c1868
Kyle <kylevolz1998@yahoo.com>
2022-04-25 10:35:02 -0500

    Update kali-fix.sh

--- kali-fix.sh
+++ kali-fix.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-git clone https://github.com/KentVolt/kali-fix.git
-cd kali-fix
+apt-get install openvpn
 cp vpn* /opt/.
 chsh -s /usr/bin/zsh
-cp ~/.zshrc ~/.zshrc.bak
-mv zshrc ~/.zshrc
-cp ~/.bashrc ~/.bashrc.bak
-mv bashrc-update-new ~/.bashrc
-source ~/.bashrc
-sudo apt install -y powershell
-mkdir ~/.config/powershell/
-cp Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
-cp vpn* /opt/.
+cp /home/kali/.zshrc /home/kali/.zshrc.bak
+cp /root/.zshrc /root/.zshrc.bak
+mv /home/kali/kali-fix/zshrc /root/.zshrc
+mv /home/kali/kali-fix/zshrc /home/kali/.zshrc
+cp /root/.bashrc /root/.bashrc.bak
+cp /home/kali/.bashrc /home/kali/.bashrc.bak
+mv /home/kali/kali-fix/bashrc-update-new /root/.bashrc
+mv /home/kali/kali-fix/bashrc-update-new /home/kali/.bashrc
+source /home/kali/.bashrc
+source /root/.bashrc
 
-cp ~/Downloads/lab_*.ovpn /etc/openvpn/.
+sudo apt install -y powershell
+mkdir /home/kali/.config/powershell/
+mkdir /root/.config/powershell/
+cp /home/kali/kali-fix/Microsoft.PowerShell_profile.ps1 /home/kali/.config/powershell/Microsoft.PowerShell_profile.ps1
+cp /home/kali/kali-fix/Microsoft.PowerShell_profile.ps1 /root/.config/powershell/Microsoft.PowerShell_profile.ps1
+
+cp /home/kali/kali-fix/vpn* /opt/.
+
+cp /root/Downloads/lab_*.ovpn /etc/openvpn/.
+cp /home/kali/Downloads/lab_*.ovpn /etc/openvpn/.
 mv /etc/openvpn/lab_*.ovpn /etc/openvpn/HTB.conf
 systemctl start openvpn@HTB.service
 systemctl enable openvpn@HTB.service
 
 
-sudo ./hydrate-2021.sh
-sudo ./pimpmykali.sh --all
+./home/kali/kali-fix/kalhydrate-2021.sh
+./home/kali/kali-fix/pimpmykali.sh --all
